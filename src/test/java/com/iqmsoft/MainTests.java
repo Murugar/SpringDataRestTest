36a5388b077897387d76f875fc3ee640699521fe
Davanon <davanon2014@gmail.com>
2018-01-22 09:18:19 +0530

    First

---
+++ src/test/java/com/iqmsoft/MainTests.java
@@ -0,0 +1,21 @@
+package com.iqmsoft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.iqmsoft.Main;
+
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Main.class)
+@WebAppConfiguration
+public class MainTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
